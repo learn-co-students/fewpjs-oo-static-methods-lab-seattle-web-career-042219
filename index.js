47b7769c987e280048db8e7830639e1022039bff
Sean Watters <sean@watters.io>
2019-06-12 15:09:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.substring(1);
+  }
+
+  static sanitize(string) {
+    string = string.replace(/[^a-z0-9 \.,_'-]/gim,"");
+    return string
+  }
+
+  static titleize(string) {
+
+    let words = string.split(' ');
+
+    string = words.map( word => {
+      if (word === 'the' || word === 'a' || word === 'an' || word === 'but' || word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by' || word === 'from') {
+        word;
+      } else {
+        word = word.charAt(0).toUpperCase() + word.substring(1);
+      }
+      return word;
+    }).join(' ');
+    return string.charAt(0).toUpperCase() + string.substring(1)
+
+  }
 }
