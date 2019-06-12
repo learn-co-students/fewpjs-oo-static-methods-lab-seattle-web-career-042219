a620d83abb595cd27cf55db0bdb64b5993b1b55a
github username <github email address>
2019-06-12 15:16:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.substr(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^\w-'\s]/gi, "")
+  }
+
+  static titleize(str) {
+    const NONCAPS = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+
+    const words = str.split(' ')
+    words[0] = Formatter.capitalize(words[0])
+    for (let i = 1; i < words.length; i++) {
+      if (!NONCAPS.includes(words[i])) {
+        words[i] = Formatter.capitalize(words[i])
+      }
+    }
+
+    return words.join(' ')
+  }
+
 }
